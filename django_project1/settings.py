d50c778d336b37a3944214d6a511aac5a9c420a1
Shraefkt <74842465+Shraefkt@users.noreply.github.com>
2020-12-17 15:20:20 +0800

    something

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 
 import os
+from pathlib import Path
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = str(Path(__file__).resolve().parent.parent)
 
 
 # Quick-start development settings - unsuitable for production
