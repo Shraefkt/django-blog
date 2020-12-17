434ad183f08607f415496d118ea9984c780390e9
Shraefkt <74842465+Shraefkt@users.noreply.github.com>
2020-12-17 14:27:22 +0800

    something

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '35d77b362df7c896f0da4a662167f7340f9f610ae8ec66b4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["veritaslibre.herokuapp.com"]
 
